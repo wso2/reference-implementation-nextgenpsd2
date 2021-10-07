50ed394c2785701038dfabba6075b22018ab15f8
Akila94 <akilaaroshanaa@gmail.com>
2021-10-07 11:31:50 +0530

    Add error construct implementation for Berlin toolkit

---
+++ components/com.wso2.openbanking.berlin.common/src/main/java/com/wso2/openbanking/berlin/common/utils/ErrorConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ *  This software is the property of WSO2 Inc. and its suppliers, if any.
+ *  Dissemination of any information or reproduction of any material contained
+ *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ *  the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
+ *  For specific language governing the permissions and limitations under this
+ *  license, please see the license as well as any agreement you’ve entered into
+ *  with WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.openbanking.berlin.common.utils;
+
+/**
+ * Constants related to errors.
+ */
+public class ErrorConstants {
+
+    // Error messages
+    public static final String X_REQUEST_ID_MISSING = "X-Request-ID header is missing in the request";
+
+    // Error object related constants
+    public static final String PATH = "path";
+    public static final String CATEGORY = "category";
+    public static final String CODE = "code";
+    public static final String TEXT = "text";
+}
