81cf6316115e88eb883052aa59634e2906204f7f
Akram Azarm <akram.arm.7@gmail.com>
2021-10-08 12:21:03 +0530

    consent validate structure

---
+++ components/com.wso2.openbanking.berlin.consent.extensions/src/main/java/com/wso2/openbanking/berlin/consent/extensions/validate/validator/factory/SubmissionValidatorFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ *  This software is the property of WSO2 Inc. and its suppliers, if any.
+ *  Dissemination of any information or reproduction of any material contained
+ *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ *  the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
+ *  For specific language governing the permissions and limitations under this
+ *  license, please see the license as well as any agreement you’ve entered into
+ *  with WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.openbanking.berlin.consent.extensions.validate.validator.factory;
+
+import com.wso2.openbanking.berlin.consent.extensions.common.ConsentExtensionConstants;
+import com.wso2.openbanking.berlin.consent.extensions.validate.validator.SubmissionValidator;
+import com.wso2.openbanking.berlin.consent.extensions.validate.validator.impl.AccountSubmissionValidator;
+import com.wso2.openbanking.berlin.consent.extensions.validate.validator.impl.CardAccountSubmissionValidator;
+import com.wso2.openbanking.berlin.consent.extensions.validate.validator.impl.FundsConfirmationSubmissionValidator;
+
+/**
+ * Factory for deciding the submission request validator.
+ */
+public class SubmissionValidatorFactory {
+
+    /**
+     * Method to get the Submission Validator instance.
+     *
+     * @param requestPath Request path of the request
+     * @return SubmissionValidator
+     */
+    public static SubmissionValidator getSubmissionValidator(String requestPath) {
+
+        if (requestPath == null) {
+            return null;
+        }
+
+        String[] requestPathArray = requestPath.split("/");
+        String[] requestPathArrayWithQuery = requestPath.split("\\?");
+
+        if (ConsentExtensionConstants.ACCOUNTS_SUBMISSION_PATH_IDENTIFIER.equals(requestPathArray[0])
+                || ConsentExtensionConstants.ACCOUNTS_SUBMISSION_PATH_IDENTIFIER.equals(requestPathArrayWithQuery[0])) {
+            return new AccountSubmissionValidator();
+        }
+
+        if (ConsentExtensionConstants.CARD_ACCOUNTS_SUBMISSION_PATH_IDENTIFIER.equals(requestPathArray[0])) {
+            return new CardAccountSubmissionValidator();
+        }
+
+        if (ConsentExtensionConstants.FUNDS_CONFIRMATIONS_SUBMISSION_PATH_IDENTIFIER.equals(requestPathArray[0])) {
+            return new FundsConfirmationSubmissionValidator();
+        }
+
+        return null;
+
+    }
+
+}
---
+++ components/com.wso2.openbanking.berlin.consent.extensions/src/main/java/com/wso2/openbanking/berlin/consent/extensions/validate/validator/impl/CardAccountSubmissionValidator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ *  This software is the property of WSO2 Inc. and its suppliers, if any.
+ *  Dissemination of any information or reproduction of any material contained
+ *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ *  the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
+ *  For specific language governing the permissions and limitations under this
+ *  license, please see the license as well as any agreement you’ve entered into
+ *  with WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.openbanking.berlin.consent.extensions.validate.validator.impl;
+
+import com.wso2.openbanking.accelerator.consent.extensions.common.ConsentException;
+import com.wso2.openbanking.accelerator.consent.extensions.validate.model.ConsentValidateData;
+import com.wso2.openbanking.accelerator.consent.extensions.validate.model.ConsentValidationResult;
+import com.wso2.openbanking.berlin.consent.extensions.validate.validator.SubmissionValidator;
+
+/**
+ * Validate Card Accounts submission requests.
+ */
+public class CardAccountSubmissionValidator implements SubmissionValidator {
+
+    @Override
+    public void validate(ConsentValidateData consentValidateData, ConsentValidationResult consentValidationResult)
+            throws ConsentException {
+
+    }
+
+}
