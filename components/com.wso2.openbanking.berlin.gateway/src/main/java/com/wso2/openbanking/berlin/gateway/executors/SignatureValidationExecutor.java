9a33bb2d03971515aadaee81bdad97c43c23a924
Akila <user@akilaa.local>
2022-01-18 13:15:56 +0530

    Comment out org ID validation until the relative bug is fixed

--- components/com.wso2.openbanking.berlin.gateway/src/main/java/com/wso2/openbanking/berlin/gateway/executors/SignatureValidationExecutor.java
+++ components/com.wso2.openbanking.berlin.gateway/src/main/java/com/wso2/openbanking/berlin/gateway/executors/SignatureValidationExecutor.java
@@ -16,8 +16,6 @@ import com.wso2.openbanking.accelerator.common.config.OpenBankingConfigParser;
 import com.wso2.openbanking.accelerator.common.constant.OpenBankingConstants;
 import com.wso2.openbanking.accelerator.common.exception.CertificateValidationException;
 import com.wso2.openbanking.accelerator.common.util.Generated;
-import com.wso2.openbanking.accelerator.common.util.eidas.certificate.extractor.CertificateContent;
-import com.wso2.openbanking.accelerator.common.util.eidas.certificate.extractor.CertificateContentExtractor;
 import com.wso2.openbanking.accelerator.gateway.cache.CertificateRevocationCache;
 import com.wso2.openbanking.accelerator.gateway.cache.GatewayCacheKey;
 import com.wso2.openbanking.accelerator.gateway.executor.core.OpenBankingGatewayExecutor;
@@ -168,61 +166,63 @@ public class SignatureValidationExecutor implements OpenBankingGatewayExecutor {
     @Override
     public void postProcessRequest(OBAPIRequestContext obapiRequestContext) {
 
+        // TODO: uncomment after https://github.com/wso2-enterprise/financial-open-banking/issues/7072 is fixed
+
         // Retrieve transport certificate from the request
-        javax.security.cert.X509Certificate[] x509Certificates = obapiRequestContext.getClientCerts();
-        javax.security.cert.X509Certificate transportCert;
-        Optional<java.security.cert.X509Certificate> convertedTransportCert;
-
-        if (x509Certificates.length != 0) {
-            transportCert = x509Certificates[0];
-            convertedTransportCert = CertificateValidationUtils.convert(transportCert);
-        } else {
-            log.error("Transport certificate not found in request context");
-            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_MISSING.toString(),
-                    "Couldn't do organization ID validation since transport certificate not found in" +
-                            " request context");
-            return;
-        }
-
-        CertificateContent content;
-
-        try {
-            // Extract certificate content
-            if (convertedTransportCert.isPresent()) {
-                content = CertificateContentExtractor.extract(convertedTransportCert.get());
-            } else {
-                log.error("Error while processing transport certificate");
-                GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
-                        "Couldn't do organization ID validation since an error occurred while processing " +
-                                "transport certificate");
-                return;
-            }
-        } catch (CertificateValidationException e) {
-            log.error("Error while extracting transport certificate content", e);
-            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
-                    "Couldn't do organization ID validation since an error occurred while extracting " +
-                            "transport certificate");
-            return;
-        }
-
-        String clientId = obapiRequestContext.getApiRequestInfo().getConsumerKey();
-        String certificateOrgId = content.getPspAuthorisationNumber();
-
-        if (StringUtils.isBlank(certificateOrgId)) {
-            log.error("Unable to retrieve organization ID from transport certificate");
-            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
-                    "An organization ID is not found in the provided certificate");
-            return;
-        } else {
-            if (!StringUtils.equals(clientId, certificateOrgId)) {
-                log.error("Client ID is not matching with the organization ID of the provided transport" +
-                        " certificate");
-                GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
-                        "Organization ID mismatch with Client ID");
-                return;
-            }
-        }
-        log.debug("Organization ID validation is completed");
+//        javax.security.cert.X509Certificate[] x509Certificates = obapiRequestContext.getClientCerts();
+//        javax.security.cert.X509Certificate transportCert;
+//        Optional<java.security.cert.X509Certificate> convertedTransportCert;
+//
+//        if (x509Certificates.length != 0) {
+//            transportCert = x509Certificates[0];
+//            convertedTransportCert = CertificateValidationUtils.convert(transportCert);
+//        } else {
+//            log.error("Transport certificate not found in request context");
+//            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_MISSING.toString(),
+//                    "Couldn't do organization ID validation since transport certificate not found in" +
+//                            " request context");
+//            return;
+//        }
+//
+//        CertificateContent content;
+//
+//        try {
+//            // Extract certificate content
+//            if (convertedTransportCert.isPresent()) {
+//                content = CertificateContentExtractor.extract(convertedTransportCert.get());
+//            } else {
+//                log.error("Error while processing transport certificate");
+//                GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
+//                        "Couldn't do organization ID validation since an error occurred while processing " +
+//                                "transport certificate");
+//                return;
+//            }
+//        } catch (CertificateValidationException e) {
+//            log.error("Error while extracting transport certificate content", e);
+//            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
+//                    "Couldn't do organization ID validation since an error occurred while extracting " +
+//                            "transport certificate");
+//            return;
+//        }
+//
+//        String clientId = obapiRequestContext.getApiRequestInfo().getConsumerKey();
+//        String certificateOrgId = content.getPspAuthorisationNumber();
+//
+//        if (StringUtils.isBlank(certificateOrgId)) {
+//            log.error("Unable to retrieve organization ID from transport certificate");
+//            GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
+//                    "An organization ID is not found in the provided certificate");
+//            return;
+//        } else {
+//            if (!StringUtils.equals(clientId, certificateOrgId)) {
+//                log.error("Client ID is not matching with the organization ID of the provided transport" +
+//                        " certificate");
+//                GatewayUtils.handleFailure(obapiRequestContext, TPPMessage.CodeEnum.CERTIFICATE_INVALID.toString(),
+//                        "Organization ID mismatch with Client ID");
+//                return;
+//            }
+//        }
+//        log.debug("Organization ID validation is completed");
     }
 
     @Override
