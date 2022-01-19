30918a4455913c7db3c728da49f426ae5c06a1af
Akila <user@akilaa.local>
2022-01-19 20:40:31 +0530

    Add frequency per day executor

---
+++ components/com.wso2.openbanking.berlin.gateway/src/main/java/com/wso2/openbanking/berlin/gateway/executors/FrequencyPerDayExecutor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ *  This software is the property of WSO2 Inc. and its suppliers, if any.
+ *  Dissemination of any information or reproduction of any material contained
+ *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ *  the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
+ *  For specific language governing the permissions and limitations under this
+ *  license, please see the license as well as any agreement you’ve entered into
+ *  with WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.openbanking.berlin.gateway.executors;
+
+import com.wso2.openbanking.accelerator.gateway.executor.core.OpenBankingGatewayExecutor;
+import com.wso2.openbanking.accelerator.gateway.executor.model.OBAPIRequestContext;
+import com.wso2.openbanking.accelerator.gateway.executor.model.OBAPIResponseContext;
+
+/**
+ * CLass to add required throttling key parameters to the messageContext in frequency per day scenarios.
+ */
+public class FrequencyPerDayExecutor implements OpenBankingGatewayExecutor {
+    
+    @Override
+    public void preProcessRequest(OBAPIRequestContext obapiRequestContext) {
+
+    }
+
+    @Override
+    public void postProcessRequest(OBAPIRequestContext obapiRequestContext) {
+
+    }
+
+    @Override
+    public void preProcessResponse(OBAPIResponseContext obapiResponseContext) {
+
+    }
+
+    @Override
+    public void postProcessResponse(OBAPIResponseContext obapiResponseContext) {
+
+    }
+}
