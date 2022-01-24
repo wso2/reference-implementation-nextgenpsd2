601b4f6b4a88408f51c24983963f84419b6f5438
Akila <user@akilaa.local>
2022-01-24 10:11:15 +0530

    Partial implementation 1

--- components/com.wso2.openbanking.berlin.gateway/src/main/java/com/wso2/openbanking/berlin/gateway/executors/FrequencyPerDayExecutor.java
+++ components/com.wso2.openbanking.berlin.gateway/src/main/java/com/wso2/openbanking/berlin/gateway/executors/FrequencyPerDayExecutor.java
@@ -15,15 +15,65 @@ package com.wso2.openbanking.berlin.gateway.executors;
 import com.wso2.openbanking.accelerator.gateway.executor.core.OpenBankingGatewayExecutor;
 import com.wso2.openbanking.accelerator.gateway.executor.model.OBAPIRequestContext;
 import com.wso2.openbanking.accelerator.gateway.executor.model.OBAPIResponseContext;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * CLass to add required throttling key parameters to the messageContext in frequency per day scenarios.
  */
 public class FrequencyPerDayExecutor implements OpenBankingGatewayExecutor {
-    
+
+    private static final Log log = LogFactory.getLog(FrequencyPerDayExecutor.class);
+
+    private static final String CUSTOM_PROPERTY = "customProperty";
+    private static final String PSU_IP_ADDRESS = "PSU-IP-Address";
+    private static final String CONSENT_ID_HEADER = "Consent-ID";
+    //Key map constants
+    private static final String ACCOUNT_ID = "accountId";
+    private static final String CONSENT_ID = "consentId";
+
+    private HashMap<String, Object> customKeyMap = new HashMap<>();
+
+    private static final List<String> eligibleResources = Arrays.asList("/accounts/{account-id}", "/accounts/{account"
+                    + "-id}?withBalance", "/accounts/{account-id}/balances", "/accounts/{account-id}/transactions",
+            "/accounts" + "/{account-id}/transactions?withBalance", "/accounts/{account-id}/transactions" +
+                    "/{transactionId}", "/card" + "-accounts/{account-id}", "/card-accounts/{account-id}/balances",
+            "/card-accounts/{account-id" + "}/transactions", "/card-accounts/{account-id}/transactions/{transactionId"
+                    + "}");
+
     @Override
     public void preProcessRequest(OBAPIRequestContext obapiRequestContext) {
 
+        Map<String, String> requestHeaders = obapiRequestContext.getMsgInfo().getHeaders();
+
+        if (/*CommonConfigParser.getInstance().isFrequencyPerDayThrottlingEnabled()
+                && */StringUtils.isNotBlank(requestHeaders.get(PSU_IP_ADDRESS))) {
+
+            String electedResource = obapiRequestContext.getMsgInfo().getElectedResource();
+            String resourceString = obapiRequestContext.getMsgInfo().getResource();
+
+            if (log.isDebugEnabled()) {
+                log.debug("Checking whether the " + resourceString + " is eligible for throttling");
+            }
+
+//            if (isEligibleForFrequencyPerDayThrottling(electedResource)) {
+
+                List<String> pathList = Arrays.asList(resourceString.split("/"));
+                String accountID = "accountID";
+                String consentID = "ConsentID";
+                customKeyMap.put(ACCOUNT_ID, accountID);
+                customKeyMap.put(CONSENT_ID, consentID);
+
+                //setting throttling property to be utilized by the throttle handler
+                obapiRequestContext.setCustomProperty(customKeyMap);
+//            }
+        }
     }
 
     @Override
@@ -39,5 +89,21 @@ public class FrequencyPerDayExecutor implements OpenBankingGatewayExecutor {
     @Override
     public void postProcessResponse(OBAPIResponseContext obapiResponseContext) {
 
+    }
+
+    /**
+     * Check if the request is eligible for frequency per day throttling.
+     *
+     * @param url elected API resource
+     * @return true if eligible; false otherwise
+     */
+    private boolean isEligibleForFrequencyPerDayThrottling(String url) {
+
+        for (String path : eligibleResources) {
+            if (path.equals(url)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
