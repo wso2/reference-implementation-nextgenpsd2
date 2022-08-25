00c7952016418209fd3720649f7c0cff7a9797f2
IsuruWijethunga <isuruwijethunga969@gmail.com>
2022-08-25 12:01:07 +0530

    Removed CDR Logo

--- react-apps/self-care-portal/self-care-portal-frontend/src/specConfigs/BG/componants/detailedAggrementPage/AccreditationBG.jsx
+++ react-apps/self-care-portal/self-care-portal-frontend/src/specConfigs/BG/componants/detailedAggrementPage/AccreditationBG.jsx
@@ -11,7 +11,7 @@
  */
 
 import React from "react";
-import CDRLogo from "../../../../images/CDRLogo.png";
+import Logo from "../../../../images/Logo.png";
 
 export const AccreditationBG = ({infoLabel, applicationName}) => {
     return (
