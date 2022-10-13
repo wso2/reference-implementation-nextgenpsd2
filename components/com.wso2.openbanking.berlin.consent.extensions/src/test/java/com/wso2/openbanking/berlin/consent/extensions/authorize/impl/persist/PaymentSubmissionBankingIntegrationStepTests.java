25f644c019849b3a0f466c59b75c72c89f2fc2e5
akila <akila@192.168.1.3>
2022-10-13 13:14:33 +0530

    Update unit tests

--- components/com.wso2.openbanking.berlin.consent.extensions/src/test/java/com/wso2/openbanking/berlin/consent/extensions/authorize/impl/persist/PaymentSubmissionBankingIntegrationStepTests.java
+++ components/com.wso2.openbanking.berlin.consent.extensions/src/test/java/com/wso2/openbanking/berlin/consent/extensions/authorize/impl/persist/PaymentSubmissionBankingIntegrationStepTests.java
@@ -9,9 +9,11 @@
 
 package com.wso2.openbanking.berlin.consent.extensions.authorize.impl.persist;
 
+import com.wso2.openbanking.accelerator.common.exception.ConsentManagementException;
 import com.wso2.openbanking.accelerator.common.exception.OpenBankingException;
 import com.wso2.openbanking.accelerator.consent.extensions.authorize.model.ConsentData;
 import com.wso2.openbanking.accelerator.consent.extensions.authorize.model.ConsentPersistData;
+import com.wso2.openbanking.accelerator.consent.extensions.common.ConsentException;
 import com.wso2.openbanking.accelerator.consent.mgt.dao.models.AuthorizationResource;
 import com.wso2.openbanking.accelerator.consent.mgt.dao.models.ConsentResource;
 import com.wso2.openbanking.accelerator.consent.mgt.service.impl.ConsentCoreServiceImpl;
@@ -25,6 +27,7 @@ import com.wso2.openbanking.berlin.consent.extensions.common.TransactionStatusEn
 import com.wso2.openbanking.berlin.consent.extensions.util.TestConstants;
 import com.wso2.openbanking.berlin.consent.extensions.util.TestPayloads;
 import com.wso2.openbanking.berlin.consent.extensions.util.TestUtil;
+import graphql.Assert;
 import net.minidev.json.JSONObject;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -111,16 +114,12 @@ public class PaymentSubmissionBankingIntegrationStepTests extends PowerMockTestC
         consentData.setConsentId(consentId);
         consentData.setAuthResource(authorizationResource);
         consentData.setConsentResource(consentResource);
-        consentData.addData(ConsentExtensionConstants.ACCOUNT_REF_OBJECT, TestUtil
-                .getSampleAccountRefObject("iban", "123", null));
         ConsentPersistData consentPersistData = new ConsentPersistData(new JSONObject(), new HashMap<>(), true,
                 consentData);
 
         doReturn(consentResource).when(consentCoreServiceMock).getConsent(Mockito.anyString(), Mockito.anyBoolean());
         doReturn(TestConstants.SUPPORTED_ACC_REF_TYPES).when(commonConfigParserMock)
                 .getSupportedAccountReferenceTypes();
-        doReturn(new AuthorizationResource()).when(consentCoreServiceMock)
-                .updateAuthorizationStatus(Mockito.anyString(), Mockito.anyString());
 
         doReturn("sampleBackendUrl")
                 .when(commonConfigParserMock).getPaymentsBackendURL();
@@ -134,5 +133,94 @@ public class PaymentSubmissionBankingIntegrationStepTests extends PowerMockTestC
         doReturn(authorizationResources).when(consentCoreServiceMock).searchAuthorizations(Mockito.anyString());
 
         paymentSubmissionBankingIntegrationStep.execute(consentPersistData);
+    }
+
+    @Test
+    public void testConsentPersistWithPaymentSubmissionFailure()
+            throws URISyntaxException, OpenBankingException, IOException {
+
+        ConsentResource consentResource =
+                TestUtil.getSampleConsentResource(TransactionStatusEnum.RCVD.toString(),
+                        ConsentTypeEnum.PAYMENTS.toString(), TestPayloads.VALID_PAYMENTS_PAYLOAD, consentId, clientId);
+
+        AuthorizationResource authorizationResource = TestUtil.getSampleStoredTestAuthorizationResource(consentId,
+                AuthTypeEnum.AUTHORISATION.toString(), ScaStatusEnum.RECEIVED.toString(), authId,
+                TestConstants.USER_ID);
+
+        ConsentData consentData = TestUtil.getSampleConsentDataObject(TestConstants.USER_ID,
+                TestConstants.SAMPLE_QUERY_PARAMS, "pis:" + consentId, TestConstants.SAMPLE_APP_NAME,
+                new HashMap<>(), true);
+
+        consentData.setType(ConsentExtensionConstants.PAYMENTS);
+        consentData.setConsentId(consentId);
+        consentData.setAuthResource(authorizationResource);
+        consentData.setConsentResource(consentResource);
+        ConsentPersistData consentPersistData = new ConsentPersistData(new JSONObject(), new HashMap<>(), true,
+                consentData);
+
+        doReturn(consentResource).when(consentCoreServiceMock).getConsent(Mockito.anyString(), Mockito.anyBoolean());
+        doReturn(TestConstants.SUPPORTED_ACC_REF_TYPES).when(commonConfigParserMock)
+                .getSupportedAccountReferenceTypes();
+
+        doReturn("sampleBackendUrl")
+                .when(commonConfigParserMock).getPaymentsBackendURL();
+        PowerMockito.when(ConsentAuthUtil.isPaymentResourceSubmitted(Mockito.anyString(), Mockito.anyString(),
+                Mockito.anyString())).thenReturn(false);
+        PowerMockito.when(ConsentAuthUtil.areAllOtherAuthResourcesValid(Mockito.any(), Mockito.anyString(),
+                Mockito.any())).thenReturn(true);
+
+        ArrayList<AuthorizationResource> authorizationResources = new ArrayList<>();
+        authorizationResources.add(authorizationResource);
+        doReturn(authorizationResources).when(consentCoreServiceMock).searchAuthorizations(Mockito.anyString());
+
+        try {
+            paymentSubmissionBankingIntegrationStep.execute(consentPersistData);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof ConsentException);
+        }
+    }
+
+    @Test
+    public void testConsentPersistWithValidData2() throws URISyntaxException, OpenBankingException, IOException {
+
+        ConsentResource consentResource =
+                TestUtil.getSampleConsentResource(TransactionStatusEnum.RCVD.toString(),
+                        ConsentTypeEnum.PAYMENTS.toString(), TestPayloads.VALID_PAYMENTS_PAYLOAD, consentId, clientId);
+
+        AuthorizationResource authorizationResource = TestUtil.getSampleStoredTestAuthorizationResource(consentId,
+                AuthTypeEnum.AUTHORISATION.toString(), ScaStatusEnum.RECEIVED.toString(), authId,
+                TestConstants.USER_ID);
+
+        ConsentData consentData = TestUtil.getSampleConsentDataObject(TestConstants.USER_ID,
+                TestConstants.SAMPLE_QUERY_PARAMS, "pis:" + consentId, TestConstants.SAMPLE_APP_NAME,
+                new HashMap<>(), true);
+
+        consentData.setType(ConsentExtensionConstants.PAYMENTS);
+        consentData.setConsentId(consentId);
+        consentData.setAuthResource(authorizationResource);
+        consentData.setConsentResource(consentResource);
+        ConsentPersistData consentPersistData = new ConsentPersistData(new JSONObject(), new HashMap<>(), true,
+                consentData);
+
+        doReturn(consentResource).when(consentCoreServiceMock).getConsent(Mockito.anyString(), Mockito.anyBoolean());
+        doReturn(TestConstants.SUPPORTED_ACC_REF_TYPES).when(commonConfigParserMock)
+                .getSupportedAccountReferenceTypes();
+
+        doReturn("sampleBackendUrl")
+                .when(commonConfigParserMock).getPaymentsBackendURL();
+        PowerMockito.when(ConsentAuthUtil.isPaymentResourceSubmitted(Mockito.anyString(), Mockito.anyString(),
+                Mockito.anyString())).thenReturn(true);
+        PowerMockito.when(ConsentAuthUtil.areAllOtherAuthResourcesValid(Mockito.any(), Mockito.anyString(),
+                Mockito.any())).thenThrow(ConsentManagementException.class);
+
+        ArrayList<AuthorizationResource> authorizationResources = new ArrayList<>();
+        authorizationResources.add(authorizationResource);
+        doReturn(authorizationResources).when(consentCoreServiceMock).searchAuthorizations(Mockito.anyString());
+
+        try {
+            paymentSubmissionBankingIntegrationStep.execute(consentPersistData);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof ConsentException);
+        }
     }
 }
