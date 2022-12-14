12780fc3ef33de46577328c4c42a885998f1150b
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2022-12-14 13:48:33 +0530

    Add worker logic for each authorization status

---
+++ components/com.wso2.openbanking.berlin.consent.extensions/src/main/java/com/wso2/openbanking/berlin/consent/extensions/authorize/worker/PSUAuthenticatedStatusUpdateWorker.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com/). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+package com.wso2.openbanking.berlin.consent.extensions.authorize.worker;
+
+import com.wso2.openbanking.berlin.consent.extensions.common.ScaStatusEnum;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Class to update authorization object to PSU Authenticated status.
+ */
+public class PSUAuthenticatedStatusUpdateWorker extends BerlinAuthStatusUpdateWorker {
+
+    @Override
+    boolean isCurrentAuthorizationStatusEligible(String authStatus) {
+        return StringUtils.equals(ScaStatusEnum.RECEIVED.toString(), authStatus) ||
+                StringUtils.equals(ScaStatusEnum.SCA_METHOD_SELECTED.toString(), authStatus) ||
+                StringUtils.equals(ScaStatusEnum.PSU_IDENTIFIED.toString(), authStatus);
+    }
+
+    @Override
+    String getNewAuthStatusValue() {
+        return ScaStatusEnum.PSU_AUTHENTICATED.toString();
+    }
+}
