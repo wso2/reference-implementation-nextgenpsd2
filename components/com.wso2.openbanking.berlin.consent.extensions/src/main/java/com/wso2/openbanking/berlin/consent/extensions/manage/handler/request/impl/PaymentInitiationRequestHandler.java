8d8c643611dace23f6697d608545ee57e203c98d
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2022-12-14 13:47:33 +0530

    update request handler classes according to the new status lifecycle

--- components/com.wso2.openbanking.berlin.consent.extensions/src/main/java/com/wso2/openbanking/berlin/consent/extensions/manage/handler/request/impl/PaymentInitiationRequestHandler.java
+++ components/com.wso2.openbanking.berlin.consent.extensions/src/main/java/com/wso2/openbanking/berlin/consent/extensions/manage/handler/request/impl/PaymentInitiationRequestHandler.java
@@ -1,13 +1,10 @@
-/*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+/**
+ * Copyright (c) 2021 - 2022, WSO2 LLC. (https://www.wso2.com/). All Rights Reserved.
  *
- *  This software is the property of WSO2 Inc. and its suppliers, if any.
- *  Dissemination of any information or reproduction of any material contained
- *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
- *  the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
- *  For specific language governing the permissions and limitations under this
- *  license, please see the license as well as any agreement you’ve entered into
- *  with WSO2 governing the purchase of this software and any associated services.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
 package com.wso2.openbanking.berlin.consent.extensions.manage.handler.request.impl;
@@ -28,7 +25,6 @@ import com.wso2.openbanking.berlin.consent.extensions.common.AuthTypeEnum;
 import com.wso2.openbanking.berlin.consent.extensions.common.ConsentExtensionConstants;
 import com.wso2.openbanking.berlin.consent.extensions.common.ConsentExtensionUtil;
 import com.wso2.openbanking.berlin.consent.extensions.common.HeaderValidator;
-import com.wso2.openbanking.berlin.consent.extensions.common.ScaStatusEnum;
 import com.wso2.openbanking.berlin.consent.extensions.common.TransactionStatusEnum;
 import com.wso2.openbanking.berlin.consent.extensions.manage.handler.request.RequestHandler;
 import com.wso2.openbanking.berlin.consent.extensions.manage.util.CommonConsentUtil;
@@ -81,7 +77,7 @@ public class PaymentInitiationRequestHandler implements RequestHandler {
             ConsentResource consentResource = new ConsentResource(consentManageData.getClientId(),
                     requestPayload.toJSONString(), paymentConsentType, TransactionStatusEnum.RCVD.name());
 
-            String authStatus = isExplicitAuth ? null : ScaStatusEnum.RECEIVED.toString();
+            String authStatus = CommonConsentUtil.getAuthorizationStatus(isSCARequired, isExplicitAuth, headersMap);
             try {
                 createdConsent = consentCoreService.createAuthorizableConsent(consentResource,
                         headersMap.get(ConsentExtensionConstants.PSU_ID_HEADER),
