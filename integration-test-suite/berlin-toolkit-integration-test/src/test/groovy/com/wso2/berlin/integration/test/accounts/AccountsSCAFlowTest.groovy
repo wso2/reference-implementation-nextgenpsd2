6a7467f26f678f0d876933a8c88ce37d8d8c898e
Pramudi03 <pramudi@wso2.com>
2023-06-21 17:43:40 +0530

    Integration test for AccountsSCAFlowTest

---
+++ integration-test-suite/berlin-toolkit-integration-test/src/test/groovy/com/wso2/berlin/integration/test/accounts/AccountsSCAFlowTest.groovy
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2023, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
+ * For specific language governing the permissions and limitations under this
+ * license, please see the license as well as any agreement you’ve entered into
+ * with WSO2 governing the purchase of this software and any associated services.
+ */
+
+package com.wso2.berlin.integration.test.accounts
+
+import com.wso2.berlin.test.framework.BGTest
+import com.wso2.berlin.test.framework.constant.AccountsConstants
+import com.wso2.berlin.test.framework.constant.BerlinConstants
+import com.wso2.berlin.test.framework.request_builder.BerlinRequestBuilder
+import com.wso2.berlin.test.framework.utility.AccountsInitiationPayloads
+import com.wso2.openbanking.test.framework.util.TestUtil
+import org.testng.Assert
+import org.testng.annotations.Test
+
+class AccountsSCAFlowTest extends BGTest{
+
+    String consentPath = AccountsConstants.CONSENT_PATH
+    String initiationPayload = AccountsInitiationPayloads.defaultInitiationPayload
+
+    @Test (groups = ["SmokeTest", "1.3.3", "1.3.6"])
+    void "TC0201001_Accounts initiation for SCA implicit accept scenario"() {
+
+        doDefaultInitiation(consentPath, initiationPayload)
+
+        Assert.assertEquals(consentResponse.statusCode(), BerlinConstants.STATUS_CODE_201)
+        Assert.assertNotNull(accountId)
+
+        Assert.assertNotNull(TestUtil.parseResponseBody(consentResponse, "_links.scaOAuth.href"))
+        Assert.assertNotNull(TestUtil.parseResponseBody(consentResponse, "_links.scaStatus.href"))
+
+        doStatusRetrieval(consentPath)
+        Assert.assertEquals(retrievalResponse.statusCode(), BerlinConstants.STATUS_CODE_200)
+        Assert.assertEquals(consentStatus, AccountsConstants.CONSENT_STATUS_RECEIVED)
+    }
+
+
+    @Test (groups = ["SmokeTest", "1.3.3", "1.3.6"],
+            dependsOnMethods = ["TC0201001_Accounts initiation for SCA implicit accept scenario"])
+    void "TC0202013_Accounts Authorization for SCA implicit accept scenario"() {
+
+        doAuthorizationFlow()
+
+        Assert.assertNotNull(automation.currentUrl.get().contains("state"))
+        Assert.assertNotNull(code)
+
+        doStatusRetrieval(consentPath)
+        Assert.assertEquals(retrievalResponse.statusCode(), BerlinConstants.STATUS_CODE_200)
+        Assert.assertEquals(consentStatus, AccountsConstants.CONSENT_STATUS_VALID)
+
+        generateUserAccessToken()
+        Assert.assertNotNull(userAccessToken)
+    }
+
+    @Test (groups = ["SmokeTest", "1.3.3", "1.3.6"],
+            dependsOnMethods = ["TC0202013_Accounts Authorization for SCA implicit accept scenario"])
+    void "TC0210011_Retrieval Request to get the details of a specific Account"() {
+
+        def response = BerlinRequestBuilder
+                .buildBasicRequest(userAccessToken)
+                .header(BerlinConstants.CONSENT_ID_HEADER, accountId)
+                .get(AccountsConstants.SPECIFIC_ACCOUNT_PATH)
+
+        Assert.assertEquals(response.getStatusCode(), BerlinConstants.STATUS_CODE_200)
+        Assert.assertNotNull(response.jsonPath().getJsonObject("account"))
+
+    }
+
+    @Test (groups = ["SmokeTest", "1.3.3", "1.3.6"], priority = 1)
+    void "TC0202014_Accounts Authorization for SCA implicit deny scenario"() {
+
+        doDefaultInitiation(consentPath, initiationPayload)
+        Assert.assertEquals(consentResponse.statusCode(), BerlinConstants.STATUS_CODE_201)
+
+        doStatusRetrieval(consentPath)
+        Assert.assertEquals(retrievalResponse.statusCode(), BerlinConstants.STATUS_CODE_200)
+        Assert.assertEquals(consentStatus, AccountsConstants.CONSENT_STATUS_RECEIVED)
+
+        doConsentDenyFlow()
+
+        Assert.assertEquals(code, "User denied the consent")
+
+        doStatusRetrieval(consentPath)
+
+        Assert.assertEquals(consentStatus, AccountsConstants.CONSENT_STATUS_REJECTED)
+    }
+
+}
