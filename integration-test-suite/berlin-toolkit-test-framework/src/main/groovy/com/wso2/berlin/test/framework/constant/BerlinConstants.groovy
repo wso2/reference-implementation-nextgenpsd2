caed80a62f247f0f533ee47f1d826f7d638df8f9
Pramudi03 <pramudi@wso2.com>
2023-06-27 02:33:17 +0530

    Add configurations to the BerlinConstants class

--- integration-test-suite/berlin-toolkit-test-framework/src/main/groovy/com/wso2/berlin/test/framework/constant/BerlinConstants.groovy
+++ integration-test-suite/berlin-toolkit-test-framework/src/main/groovy/com/wso2/berlin/test/framework/constant/BerlinConstants.groovy
@@ -1,18 +1,50 @@
 /*
- * Copyright (c) 2023, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
- *
- * This software is the property of WSO2 Inc. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein is strictly forbidden, unless permitted by WSO2 in accordance with
- * the WSO2 Software License available at https://wso2.com/licenses/eula/3.1.
- * For specific language governing the permissions and limitations under this
- * license, please see the license as well as any agreement you’ve entered into
- * with WSO2 governing the purchase of this software and any associated services.
- */
+
+Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+This software is the property of WSO2 LLC. and its suppliers, if any.
+Dissemination of any information or reproduction of any material contained
+herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+You may not alter or remove any copyright or other notice from copies of this content.
+*/
 
 package com.wso2.berlin.test.framework.constant
 
-class BerlinConstants {
+import com.wso2.berlin.test.framework.configuration.ConfigParser
+import com.wso2.openbanking.test.framework.constant.OBConstants
+
+class BerlinConstants extends OBConstants {
+
+    static config = ConfigParser.getInstance()
+    static API_VERSION = config.getApiVersion()
+    static final String regularAcc = "DE98765432109876543210"
+    public static String AISP_PATH = getConsentPath()
+    static final String ACCOUNTS_PATH = AISP_PATH + "accounts"
+    static final String CONSENT_PATH = AISP_PATH + "consents"
+    static final String SPECIFIC_ACCOUNT_PATH = ACCOUNTS_PATH + "/" + regularAcc
+    static final String CONSENT_STATUS_RECEIVED = "received"
+    static final String CONSENT_STATUS_VALID = "valid"
+    static final String CONSENT_STATUS_REJECTED = "rejected"
+    static final String CONSENT_ID = "consentId"
+    static final String CONSENT_STATUS = "consentStatus"
+
+
+
+    static String getConsentPath() {
+
+        def aispPath
+
+        if (API_VERSION.equalsIgnoreCase("1.1.0")) {
+            aispPath = "AccountsInfoAPI/v1.1.0/"
+
+        } else if (API_VERSION.equalsIgnoreCase("1.3.3")) {
+            aispPath = "xs2a/1.3.3/"
+
+        } else if (API_VERSION.equalsIgnoreCase("1.3.6")) {
+            aispPath = "xs2a/v1/"
+        }
+        return aispPath
+    }
+
     static final String CURRENT_ACCOUNT = "DE98765432109876543210"
     static final enum AUTH_METHOD {
         PRIVATE_KEY_JWT
@@ -38,39 +70,6 @@ class BerlinConstants {
             return "${consentScope}:${consentId}"
         }
     }
-    public static final String ACCESS_TOKEN_CONTENT_TYPE = "application/x-www-form-urlencoded";
-    public static final String TOKEN_ENDPOINT = "/oauth2/token";
-    public static final String CLIENT_ID = "client_id";
-    public static final String GRANT_TYPE_KEY = "grant_type";
-    public static final String SCOPE_KEY = "scope";
-    public static final String CLIENT_ASSERTION_TYPE_KEY = "client_assertion_type";
-    public static final String CLIENT_ASSERTION_KEY = "client_assertion";
-    public static final String REDIRECT_URI_KEY = "redirect_uri";
-    public static final ArrayList<String> ACCOUNTS_DEFAULT_SCOPES = new ArrayList<>(Arrays.asList("accounts", "openid"));
-    public static final String CLIENT_ASSERTION_TYPE = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
-    public static final String CLIENT_CREDENTIALS = "client_credentials";
-    public static final String ISSUER_KEY = "iss";
-    public static final String SUBJECT_KEY = "sub";
-    public static final String AUDIENCE_KEY = "aud";
-    public static final String EXPIRE_DATE_KEY = "exp";
-    public static final String ISSUED_AT_KEY = "iat";
-    public static final String JTI_KEY = "jti";
-    public static final String CONTENT_TYPE_APPLICATION_JWT = "application/jwt";
-
-
-    static final int STATUS_CODE_201 = 201
-    static final int STATUS_CODE_200 = 200
-
-
-    public static final String X_REQUEST_ID = "X-Request-ID"
-    static final String PSU_IP_ADDRESS = "PSU-IP-Address"
-    public static final String Date = "Date"
-    static final String PSU_ID = "PSU-ID"
-    static final String PSU_TYPE = "PSU-ID-Type"
-    static final String CONSENT_ID_HEADER = "Consent-ID"
-
-
-    public static final String AUTHORIZATION_HEADER_KEY = "Authorization";
 
     public static final String TPP_REDIRECT_URI_HEADER = "TPP-Redirect-URI";
     public static final String DIGEST = "Digest";
@@ -79,10 +78,6 @@ class BerlinConstants {
     public static final String PSU_CORPORATE_ID_HEADER = "PSU-Corporate-ID";
 
     public static final String SIGNATURE = "Signature";
-
-    public static final String INVALID_KEYSTORE_PASSWORD = "wso2carbon";
-    public static final String PATH_TO_INVALID_KEYSTORE = "./../../../../../test-artifacts/" +
-            "tpp3-invalid-info/certs/signing/tpp3-invalid-signing.jks";
 
     public static final String PSU_ID_VALUE = "PSU-ID";
 
@@ -95,5 +90,26 @@ class BerlinConstants {
     static final String ACCOUNTS_SUBMIT_XPATH = """//*[@id="approve"]"""
     static final String ACCOUNTS_DENY_XPATH = """//*[@id="oauth2_authz_confirm"]/div/div[3]/div/input[2]"""
 
+    public static final String X_REQUEST_ID = "X-Request-ID"
+    static final String PSU_IP_ADDRESS = "PSU-IP-Address"
+    public static final String Date = "Date"
+    static final String PSU_ID = "PSU-ID"
+    static final String PSU_TYPE = "PSU-ID-Type"
+    static final String CONSENT_ID_HEADER = "Consent-ID"
 
+    public static final String FIREFOX_DRIVER_NAME = "webdriver.gecko.driver";
+    public static final String CHROME_DRIVER_NAME = "webdriver.chrome.driver";
+    public static final String LBL_SMSOTP_AUTHENTICATOR = "//h2[text()='Authenticating with SMSOTP']";
+    public static final String TXT_OTP_CODE = "OTPcode";
+    public static final String BTN_AUTHENTICATE = "//input[@id='authenticate']";
+    public static final String AUTH_SIGNIN_XPATH = "//*[contains(@type,'submit')]";
+
+    //Non-Regulatory
+    public static final ArrayList<String> SCOPES_OPEN_ID = new ArrayList<>(Arrays.asList("openid"));
+    public static final String USERNAME_FIELD_ID = "usernameUserInput";
+
+    public static String CLIENT_ID = "CLIENT_ID"
+    public static String APP_ACCESS_TKN = "APP_ACCESS_TKN"
+    public static String USER_ACCESS_TKN = "USER_ACCESS_TKN"
 }
+
