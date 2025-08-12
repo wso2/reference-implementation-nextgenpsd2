39bb9ccc30b2f4afa31b3560eb9411f3a978839e
Athulya Weerakoon <55914834+AthulyaWeerakoon@users.noreply.github.com>
2025-08-12 15:06:56 +0530

    Remove poorly phrased exception comments

--- components/reference-implementation-openbanking-nextgenpsd2/src/main/java/org/wso2/openbanking/nextgenpsd2/extensions/handlers/impl/ConsentAuthorisationManageHandler.java
+++ components/reference-implementation-openbanking-nextgenpsd2/src/main/java/org/wso2/openbanking/nextgenpsd2/extensions/handlers/impl/ConsentAuthorisationManageHandler.java
@@ -93,7 +93,6 @@ public class ConsentAuthorisationManageHandler implements ConsentInitiationHandl
     @Override
     public SuccessResponseForResponseAlternation enrichCreationResponse(EnrichConsentCreationRequestBody requestBody)
             throws ExtensionException {
-        // Throws an error since this should be unreachable
         throw new ExtensionException(Response.Status.BAD_REQUEST, "invalid_request",
                 ErrorConstants.AUTH_CREATION_NOT_SUPPORTED);
     }
